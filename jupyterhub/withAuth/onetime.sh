97e69981b4f30b6f5125acbb6f172059ef3cbe0a
KingLu <lushize@gmail.com>
2023-11-25 16:51:25 +0800

    解决逻辑错误

--- jupyterhub/withAuth/onetime.sh
+++ jupyterhub/withAuth/onetime.sh
@@ -1,39 +1,48 @@
 #!/bin/bash
-# 定义一个标志文件的路径
-FLAG_FILE="/root/.users_created"
-
-# 检查标志文件是否存在
-if [ -f "$FLAG_FILE" ]; then
-    echo "User creation already completed in a previous run."
-    exit 0
-fi
 
 # 从环境变量读取 JUPYTERHUB_WHITELIST
 JUPYTERHUB_WHITELIST=${JUPYTERHUB_WHITELIST:-}
 
 # 检查 JUPYTERHUB_WHITELIST 是否为空
 if [ -z "$JUPYTERHUB_WHITELIST" ]; then
-    echo "JUPYTERHUB_WHITELIST is empty or not set."
+    echo "JUPYTERHUB_WHITELIST 为空或未设置。"
+    /opt/conda/bin/jupyterhub -f /opt/jupyterhub/config/jupyterhub_config.py
     exit 1
 fi
 
 # 遍历逗号分隔的用户名
 IFS=',' read -ra USER_LIST <<< "$JUPYTERHUB_WHITELIST"
 for username in "${USER_LIST[@]}"; do
+    # 检查用户是否已经存在
+    if id "$username" &>/dev/null; then
+        echo "用户 $username 已经存在。"
+        continue
+    fi
+
     # 创建用户并指定 home 目录
-    echo "Creating user $username with home directory /home/$username..."
+    echo "正在创建用户 $username，家目录为 /home/$username..."
     useradd -m -d "/home/$username" "$username"
-    chmod 755 "/home/$username"
-    chown "$username:users" -R "/home/$username"
+
     cp -r /root/.pip /home/$username/
     cp -r /root/.jupyter /home/$username/
+    chmod 755 "/home/$username"
+    chown "$username:users" -R "/home/$username"
+
+    # 检查白名单文件是否存在，如果不存在则创建
+    WHITELIST_FILE="/home/jupyterhub_whitelist"
+    if [ ! -f "$WHITELIST_FILE" ]; then
+        touch "$WHITELIST_FILE"
+        chmod 666 "$WHITELIST_FILE"
+    fi
+
+    # 将新用户追加到白名单文件中
+    if ! grep -q "^$username$" "$WHITELIST_FILE"; then
+        echo "$username" >> "$WHITELIST_FILE"
+    fi
+
 done
 
-# 创建标志文件，表示用户已被创建
-touch "$FLAG_FILE"
+echo "用户创建完成。"
 
-echo "User creation completed."
-
+# 启动 JupyterHub
 /opt/conda/bin/jupyterhub -f /opt/jupyterhub/config/jupyterhub_config.py
-
-
